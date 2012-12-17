cc9360dc07ce47ef8c5edd37fe9589348b2ce932
nobuoka <nobuoka@vividcode.info>
2012-12-17 20:48:27 +0900

    新規プロジェクト 「ナビゲーション アプリケーション」
    Visual Studio Express 2012 for Windows 8 で新規プロジェクトを作成し,
    MeteorLine_TemporaryKey.pfx を削除してコミット. MeteorLine_TemporaryKey.pfx
    ファイルの削除だけでなく, それに合わせて MeteorLine.jsproj も変更.

---
+++ MeteorLine/pages/home/home.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // この関数は、ユーザーがこのページに移動するたびに呼び出されます。
+        // ページ要素にアプリケーションのデータを設定します。
+        ready: function (element, options) {
+            // TODO: ここでページを初期化します。
+        }
+    });
+})();
