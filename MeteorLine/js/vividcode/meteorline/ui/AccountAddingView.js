259e1852ebc5a18cc09776296cf779f2c1a7b8a3
nobuoka <nobuoka@vividcode.info>
2012-12-21 01:36:00 +0900

    AccountAddingView の中身を変更して home に表示するようにした
    Twitter アカウント追加用のフォーム部品を表示するようにして (処理は未実装),
    home に AccountAddingView を追加.

--- MeteorLine/js/vividcode/meteorline/ui/AccountAddingView.js
+++ MeteorLine/js/vividcode/meteorline/ui/AccountAddingView.js
@@ -3,7 +3,17 @@
 (function () {
     "use strict";
 
-    WinJS.UI.Pages.define("/js/vividcode/meteorline/ui/AccountAddingView.html", {
+    var AccountAddingView = WinJS.UI.Pages.define("/js/vividcode/meteorline/ui/AccountAddingView.html", {
+        /// <field type="HTMLElement">この PageControl をホストする HTML 要素</field>
+        element: null, // IntelliSense のために宣言しているだけ; 値の設定は PageControl のコンストラクタで行われる
+
+        // 初期化処理: コンストラクタで行うような処理はここで実行する
+        init: function (element, options) {
+            element.classList.add("account-adding-view-component");
+            if (!options) options = {};
+            if (options.initHide) this.hide();
+        },
+
         // この関数は、ユーザーがこのページに移動するたびに呼び出されます。
         // ページ要素にアプリケーションのデータを設定します。
         ready: function (element, options) {
@@ -14,10 +24,21 @@
             // TODO: このページからの移動に対応します。
         },
 
+        show: function () {
+            this.element.classList.remove("hide");
+        },
+        hide: function () {
+            this.element.classList.add("hide");
+        },
+
         updateLayout: function (element, viewState, lastViewState) {
             /// <param name="element" domElement="true" />
 
             // TODO: viewState の変更に対応します。
         }
+    });
+
+    WinJS.Namespace.define("vividcode.meteorline.ui", {
+        AccountAddingView: AccountAddingView
     });
 })();
--- MeteorLine/pages/home/home.js
+++ MeteorLine/pages/home/home.js
@@ -5,15 +5,15 @@
         this.element = element;
         // TODO 今は HTML 要素を直接操作しているが, 将来的には JS のオブジェクトを扱うように
         this._timelinesContainerView = element.getElementsByClassName("timelines-container-view").item(0);
-        this._accountAddingView = element.getElementsByClassName("account-adding-view").item(0);
+        this._accountAddingView = element.getElementsByClassName("account-adding-view").item(0).winControl;
     }, {
         showTimelinesContainerView: function () {
-            this._accountAddingView.style.display = "none";
+            this._accountAddingView.hide();
             this._timelinesContainerView.style.display = "";
         },
         showAccountAddingView: function () {
             this._timelinesContainerView.style.display = "none";
-            this._accountAddingView.style.display = "";
+            this._accountAddingView.show();
         }
     });
 
@@ -33,6 +33,16 @@
         },
 
         unload: function () {
+            // 終了処理
+            // ページ内に埋め込んだ PageControl の unload を呼び出す
+            var pagecontrols = [];
+            var pagecontrolElems = this.element.querySelectorAll(".homepage > section > .container > .pagecontrol");
+            for (var i = 0, len = pagecontrolElems.length; i < len; ++i) {
+                pagecontrols.push(pagecontrolElems.item(i).winControl);
+            }
+            pagecontrols.forEach(function (pagecontrol) {
+                if (pagecontrol && pagecontrol.unload) pagecontrol.unload();
+            });
         }
     });
 })();
