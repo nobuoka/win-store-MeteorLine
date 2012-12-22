9a90fdfd0427b71a00eca503eea1b564f8a56e05
nobuoka <nobuoka@vividcode.info>
2012-12-22 17:47:05 +0900

    設定のための JS ファイルを追加

---
+++ MeteorLine/js/vividcode/meteorline/Config.js
@@ -0,0 +1,16 @@
+﻿
+/// <reference path="ms-appx://Microsoft.WinJS.1.0/js/base.js" />
+(function () {
+    "use strict";
+
+    var Config = {
+        twitter: {
+            consumerKey: "YOUR_CONSUMER_KEY",
+            consumerSecret: "YOUR_CONSUMER_SECRET"
+        }
+    };
+
+    WinJS.Namespace.define("vividcode.meteorline", {
+        Config: Config
+    });
+}).call(this);
