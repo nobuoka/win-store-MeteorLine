1a2d4a641743cda8c9cc794ecff89e5daec61b60
nobuoka <nobuoka@vividcode.info>
2012-12-23 02:55:48 +0900

    タイムライン一覧用の PageControl を新規作成

---
+++ MeteorLine/js/vividcode/meteorline/ui/TimelinesContainerView.js
@@ -0,0 +1,23 @@
+﻿// ページ コントロール テンプレートの概要については、次のドキュメントを参照してください:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/js/vividcode/meteorline/ui/TimelinesContainerView.html", {
+        // この関数は、ユーザーがこのページに移動するたびに呼び出されます。
+        // ページ要素にアプリケーションのデータを設定します。
+        ready: function (element, options) {
+            // TODO: ここでページを初期化します。
+        },
+
+        unload: function () {
+            // TODO: このページからの移動に対応します。
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: viewState の変更に対応します。
+        }
+    });
+})();
